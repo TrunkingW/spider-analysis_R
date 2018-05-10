0a3e3f9be263f93f7bc0d3c05ebf2dc308e10d3b
TrunkingW <st82465@gmail.com>
2018-05-10 14:18:03 +0800

    analysis

--- summary.R
+++ summary.R
@@ -304,51 +304,78 @@ GetStockData <- function(code,DayT)
   
   DBuffer = 12*DayT
   DBuffer = gsub("-","",rev(Sys.Date() %m-% months(c(0:DBuffer))))
-  
-  for (j in 1:length(DBuffer))
-  {
-    
-    x <- paste("http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=",DBuffer[j],"&stockNo=",code,sep="")
-    x <- getURL(x)
-    x <- fromJSON(x)
-    
-    for (i in 1:length(x$data))
+  DBuffer = DBuffer[1:length(DBuffer[])-1] #¨Ò¥~´ú¸Õ²K¥[
+ 
+  tryCatch({
+   
+    for (j in 1:length(DBuffer))
     {
       
-      x$data[[i]][2] <- as.numeric(gsub(",","",x$data[[i]][2]))
-      x$data[[i]][3] <- as.numeric(gsub(",","",x$data[[i]][3]))
+      x <- paste("http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=",DBuffer[j],"&stockNo=",code,sep="")
+      x <- getURL(x)
+      x <- fromJSON(x)
       
-      
-      if (x$data[[i]][8] == "X0.00")
+      for (i in 1:length(x$data))
       {
         
-        x$data[[i]][8] = 0.00
+        x$data[[i]][2] <- as.numeric(gsub(",","",x$data[[i]][2]))
+        x$data[[i]][3] <- as.numeric(gsub(",","",x$data[[i]][3]))
+        
+  
+        if (x$data[[i]][8] == "X0.00")
+        {
+          
+          x$data[[i]][8] = 0.00
+          
+        }
+          
+        if(x$data[[i]][2] == "--" || x$data[[i]][3] == "--" || x$data[[i]][4] == "--" || x$data[[i]][5] == "--" || x$data[[i]][6] == "--" || x$data[[i]][7] == "--" || x$data[[i]][8] == "--" || x$data[[i]][9] == "--")
+        {
+          
+          x$data[[i]][2] = 0.00
+          x$data[[i]][3] = 0.00
+          x$data[[i]][4] = 0.00
+          x$data[[i]][5] = 0.00
+          x$data[[i]][6] = 0.00
+          x$data[[i]][7] = 0.00
+          x$data[[i]][8] = 0.00
+          x$data[[i]][9] = 0.00
+          
+        }
+        
+        if (n>1)
+        {
+          
+          x$data[[i]][8] <- paste(as.numeric(x$data[[i]][8]) / as.numeric(gsub(",","",Buffer[n-1,3])),"",sep="")
+          Buffer[n-1,9] <- x$data[[i]][5]
+          Buffer <- rbind(Buffer,c(x$data[[i]][1],x$data[[i]][4],x$data[[i]][7],x$data[[i]][6],x$data[[i]][5],x$data[[i]][2],x$data[[i]][3],x$data[[i]][8],x$data[[i]][9]))
+          
+        }
+        else
+        {
+          
+          Buffer <- matrix(c(x$data[[i]][1],x$data[[i]][4],x$data[[i]][7],x$data[[i]][6],x$data[[i]][5],x$data[[i]][2],x$data[[i]][3],x$data[[i]][8],x$data[[i]][9]), ncol = 9)
+          
+        }  
+        
+        n = n + 1
         
       }
       
-      if (n>1)
-      {
-        
-        x$data[[i]][8] <- paste(as.numeric(x$data[[i]][8]) / as.numeric(gsub(",","",Buffer[n-1,3])),"",sep="")
-        Buffer[n-1,9] <- x$data[[i]][5]
-        Buffer <- rbind(Buffer,c(x$data[[i]][1],x$data[[i]][4],x$data[[i]][7],x$data[[i]][6],x$data[[i]][5],x$data[[i]][2],x$data[[i]][3],x$data[[i]][8],x$data[[i]][9]))
-        
-      }
-      else
-      {
-        
-        Buffer <- matrix(c(x$data[[i]][1],x$data[[i]][4],x$data[[i]][7],x$data[[i]][6],x$data[[i]][5],x$data[[i]][2],x$data[[i]][3],x$data[[i]][8],x$data[[i]][9]), ncol = 9)
-        
-      }  
-      
-      n = n + 1
+      Sys.sleep(35)
       
     }
-    
-    Sys.sleep(10)
-    
-  }
   
+  }, warning = function(w) {
+    
+    
+  }, error = function(e) {
+    
+    Buffer <- matrix(c(0, 0, 0, 0, 0, 0, 0, 0, 0), ncol = 9)
+    Buffer <- rbind(Buffer,c(0, 0, 0, 0, 0, 0, 0, 0, 0))
+    
+  })
+    
   return(Buffer)
   
 }
